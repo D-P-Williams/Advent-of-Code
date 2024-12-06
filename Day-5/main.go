78b08cd2f6e0de7f5c58ae445948345228ec17ae
D-P-Williams <daniel@danielwilliams.me.uk>
2024-12-06 21:27:11 +0000

    Day 5

---
+++ Day-5/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func errorCheck(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	lines := readLines("./input.txt")
+	dividerIdx := slices.Index(lines, "")
+
+	rules := lines[:dividerIdx]
+	updates := lines[dividerIdx+1:]
+
+	invalidUpdates := []string{}
+
+	countPt1 := 0
+	countPt2 := 0
+
+	for _, update := range updates {
+		updateRow := strings.Split(update, ",")
+
+		valid := checkUpdateValidity(rules, updateRow)
+
+		if valid {
+			middleValue, err := strconv.ParseInt(updateRow[len(updateRow)/2], 10, 0)
+			errorCheck(err)
+
+			countPt1 += int(middleValue)
+		} else {
+			invalidUpdates = append(invalidUpdates, update)
+		}
+	}
+
+	fmt.Println("part 1", countPt1)
+
+	countPt2 = correctUpdate(rules, invalidUpdates)
+
+	fmt.Println("part 2", countPt2)
+}
+
+func checkUpdateValidity(rules []string, update []string) bool {
+	for _, rule := range rules {
+		ruleValues := strings.Split(rule, "|")
+
+		idxA := slices.Index(update, ruleValues[0])
+		idxB := slices.Index(update, ruleValues[1])
+
+		if idxA == -1 || idxB == -1 {
+			continue
+		}
+
+		if idxA > idxB {
+			return false
+		}
+	}
+
+	return true
+}
+
+func correctUpdate(rules []string, updates []string) (r int) {
+	comparison := func(a, b string) int {
+		for _, rule := range rules {
+			if rule := strings.Split(rule, "|"); rule[0] == a && rule[1] == b {
+				return -1
+			}
+		}
+
+		return 0
+	}
+
+	for _, update := range updates {
+		if update := strings.Split(update, ","); !slices.IsSortedFunc(update, comparison) {
+			slices.SortFunc(update, comparison)
+			n, _ := strconv.Atoi(update[len(update)/2])
+			r += n
+		}
+
+	}
+
+	return r
+}
---
+++ Day-5/readLines.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func readLines(path string) []string {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if scanner.Err() != nil {
+		panic(scanner.Err())
+	}
+
+	return lines
+}
