4d93778410f30fde8204ff076f89b5e458514498
D-P-Williams <daniel@danielwilliams.me.uk>
2024-12-07 10:53:47 +0000

    Add readlines to root

---
+++ Day-7/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	lines := readLines("./input.txt")
+
+	countPt1 := 0
+
+	fmt.Println("part 1", countPt1)
+
+	countPt2 := 0
+
+	fmt.Println("part 2", countPt2)
+}
