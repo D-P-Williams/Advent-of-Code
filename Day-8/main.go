174ee86804fdbc0b32545800156bcf6b7a68e2e9
D-P-Williams <daniel@danielwilliams.me.uk>
2024-12-08 16:03:20 +0000

    Day 8

--- Day-8/main.go
+++ Day-8/main.go
@@ -6,13 +6,20 @@ import (
 	aoc "github.com/D-P-Williams/Advent-of-Code-24"
 )
 
+type coord struct {
+	row int
+	col int
+}
+
+var nodesWithAntiNode = map[coord]bool{}
+
 func main() {
 	grid := aoc.ReadGrid("./input.txt")
 
 	countPt1 := 0
 	countPt2 := 0
 
-	grid, _ = processGrid(grid)
+	grid, _ = processGrid(grid, false)
 
 	for _, row := range grid {
 		fmt.Println(row)
@@ -29,85 +36,28 @@ func main() {
 
 	fmt.Println("part 1", countPt1)
 
+	// Reset map
+	nodesWithAntiNode = map[coord]bool{}
+
+	grid, _ = processGrid(grid, true)
+
+	for _, row := range grid {
+		fmt.Println(row)
+		for _, cell := range row {
+			if cell == "#" {
+				countPt2 += 1
+			}
+		}
+	}
+
+	for range nodesWithAntiNode {
+		countPt2 += 1
+	}
+
 	fmt.Println("part 2", countPt2)
 }
 
-// func processGrid(grid [][]string) (count int) {
-// 	for rowIdx, row := range grid {
-// 		for colIdx := range row {
-// 			antennaValue := grid[rowIdx][colIdx]
-
-// 			// Skip empty cells
-// 			if antennaValue == "." || antennaValue == "#" {
-// 				continue
-// 			}
-
-// 			count += findAntiNodes(grid, antennaValue, rowIdx, colIdx)
-// 		}
-// 	}
-
-// 	return count
-// }
-
-// func findAntiNodes(grid [][]string, target string, currRowIdx, currColIdx int) (count int) {
-// 	for rowIdx, row := range grid {
-// 		for colIdx := range row {
-// 			// Skip self
-// 			if rowIdx == currRowIdx && colIdx == currColIdx {
-// 				continue
-// 			}
-
-// 			if grid[rowIdx][colIdx] == target {
-// 				count += addAntiNode(grid, currRowIdx, currColIdx, rowIdx, colIdx)
-// 			}
-// 		}
-// 	}
-
-// 	return count
-// }
-
-// func addAntiNode(grid [][]string, currRowIdx, currColIdx, rowIdx, colIdx int) (count int) {
-// 	diffRow := currRowIdx - rowIdx
-// 	diffCol := currColIdx - colIdx
-
-// 	antiNode1RowIdx := currRowIdx + diffRow
-// 	antiNode1ColIdx := currColIdx + diffCol
-
-// 	antiNode2RowIdx := currRowIdx - 2*diffRow
-// 	antiNode2ColIdx := currColIdx - 2*diffCol
-
-// 	count += setAntiNode(grid, antiNode1RowIdx, antiNode1ColIdx)
-// 	count += setAntiNode(grid, antiNode2RowIdx, antiNode2ColIdx)
-
-// 	return count
-// }
-
-// func setAntiNode(grid [][]string, row, col int) (count int) {
-// 	// Early return if out of bounds
-// 	if 0 > row || row >= len(grid) {
-// 		return count
-// 	}
-
-// 	if 0 > col || col >= len(grid[0]) {
-// 		return count
-// 	}
-
-// 	// Early return if non empty cell
-// 	if grid[row][col] != "." {
-// 		return count
-// 	}
-
-// 	count += 1
-
-// 	return count
-// }
-
-var nodesWithAntiNode = map[struct {
-	row int
-	col int
-}]bool{}
-
-func processGrid(grid [][]string) ([][]string, int) {
+func processGrid(grid [][]string, part2 bool) ([][]string, int) {
 	antiUnderNode := 0
 
 	for rowIdx, row := range grid {
@@ -119,14 +69,14 @@ func processGrid(grid [][]string) ([][]string, int) {
 				continue
 			}
 
-			grid, antiUnderNode = findAntiNodes(grid, antiUnderNode, antennaValue, rowIdx, colIdx)
+			grid, antiUnderNode = findAntiNodes(grid, antiUnderNode, antennaValue, rowIdx, colIdx, part2)
 		}
 	}
 
 	return grid, antiUnderNode
 }
 
-func findAntiNodes(grid [][]string, antiUnderNode int, target string, currRowIdx, currColIdx int) ([][]string, int) {
+func findAntiNodes(grid [][]string, antiUnderNode int, target string, currRowIdx, currColIdx int, part2 bool) ([][]string, int) {
 	for rowIdx, row := range grid {
 		for colIdx := range row {
 			// Skip self
@@ -135,7 +85,7 @@ func findAntiNodes(grid [][]string, antiUnderNode int, target string, currRowIdx
 			}
 
 			if grid[rowIdx][colIdx] == target {
-				grid, antiUnderNode = addAntiNode(grid, antiUnderNode, currRowIdx, currColIdx, rowIdx, colIdx)
+				grid, antiUnderNode = addAntiNode(grid, antiUnderNode, currRowIdx, currColIdx, rowIdx, colIdx, part2)
 			}
 		}
 	}
@@ -143,18 +93,26 @@ func findAntiNodes(grid [][]string, antiUnderNode int, target string, currRowIdx
 	return grid, antiUnderNode
 }
 
-func addAntiNode(grid [][]string, antiUnderNode, currRowIdx, currColIdx, rowIdx, colIdx int) ([][]string, int) {
+func addAntiNode(grid [][]string, antiUnderNode, currRowIdx, currColIdx, rowIdx, colIdx int, part2 bool) ([][]string, int) {
 	diffRow := currRowIdx - rowIdx
 	diffCol := currColIdx - colIdx
 
-	antiNode1RowIdx := currRowIdx + diffRow
-	antiNode1ColIdx := currColIdx + diffCol
+	var multipliers []int
 
-	antiNode2RowIdx := currRowIdx - 2*diffRow
-	antiNode2ColIdx := currColIdx - 2*diffCol
+	if part2 {
+		for i := -1000; i < 1000; i++ {
+			multipliers = append(multipliers, i)
+		}
+	} else {
+		multipliers = []int{-1, 2}
+	}
 
-	grid, antiUnderNode = setAntiNode(grid, antiUnderNode, antiNode1RowIdx, antiNode1ColIdx)
-	grid, antiUnderNode = setAntiNode(grid, antiUnderNode, antiNode2RowIdx, antiNode2ColIdx)
+	for _, multiplier := range multipliers {
+		antiNodeRowIdx := currRowIdx + multiplier*diffRow
+		antiNodeColIdx := currColIdx + multiplier*diffCol
+
+		grid, antiUnderNode = setAntiNode(grid, antiUnderNode, antiNodeRowIdx, antiNodeColIdx)
+	}
 
 	return grid, antiUnderNode
 }
@@ -176,10 +134,7 @@ func setAntiNode(grid [][]string, antiUnderNode, row, col int) ([][]string, int)
 
 	// If non-empty, non-antiNode, cell (i.e. a node), increment antiUnderNode
 	if grid[row][col] != "." {
-		nodesWithAntiNode[struct {
-			row int
-			col int
-		}{row: row, col: col}] = true
+		nodesWithAntiNode[coord{row: row, col: col}] = true
 		return grid, antiUnderNode
 	}
 
