1f7b175adec8dc143a439f156507434072b983ff
D-P-Williams <daniel@danielwilliams.me.uk>
2024-12-10 18:12:38 +0000

    Add ReadIntGrid

--- utils.go
+++ utils.go
@@ -28,8 +28,8 @@ func ReadLines(path string) []string {
 	return lines
 }
 
-// readLines reads a whole file into memory
-// and returns a slice of its lines.
+// readGrid reads a whole file into memory and returns
+// a slice of its lines, split into individual strings.
 func ReadGrid(path string) [][]string {
 	lines := ReadLines(path)
 
@@ -47,6 +47,26 @@ func ReadGrid(path string) [][]string {
 	return grid
 }
 
+// readIntGrid reads a whole file into memory and returns
+// a slice of its lines, split into individual ints.
+func ReadIntGrid(path string) [][]int {
+	lines := ReadLines(path)
+
+	var grid = make([][]int, 0, len(lines))
+
+	for _, line := range lines {
+		cells := make([]int, 0, len(line))
+
+		for _, cell := range line {
+			cells = append(cells, ParseInt(string(cell)))
+		}
+
+		grid = append(grid, cells)
+	}
+	return grid
+}
+
+// ParseInt parses a string as an int and panics on error
 func ParseInt(int string) int {
 	value, err := strconv.Atoi(int)
 	if err != nil {
